ccd2f17374bba93ab58af0c5d31d37817d0af52f
Katya1539 <erkenovakatya@mail.ru>
2024-03-15 21:43:53 +0300

    main.java programm

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
+    public Main() {
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int account = 100;
+        int refill = 300;
+
+            int bonus;
+            if (refill > 1000) {
+                bonus = refill / 100 + account / 100;
+            } else {
+                bonus = 0;
+            }
+
+        System.out.println(" Итоговый бонус " + bonus);
+
+            int result = account + refill + bonus;
+
+        System.out.println(" Сумма на счету " + result);
+
+
+        }
     }
 }
